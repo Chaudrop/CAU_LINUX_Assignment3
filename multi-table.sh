a7a82cb1fdfd82b09e128742295bf691fef63753
Pierre <pierre.chaudron@epitech.eu>
2020-09-29 14:27:55 +0200

    feat(table): added multiplication table computing

--- multi-table.sh
+++ multi-table.sh
@@ -11,3 +11,13 @@ COLUMNS=$2
 if [[ ${ROWS} -lt 1 ]] || [[ ${COLUMNS} -lt 1 ]] ; then
     err "The number of rows and columns must be positive numbers."
 fi
+
+for row in $(seq 1 ${ROWS}) ; do
+    for col in $(seq 1 ${COLUMNS}) ; do
+        if [[ ${col} -gt 1 ]] ; then
+            echo -n " "
+        fi
+        echo -n "${row}*${col}=$(( ${row} * ${col} ))"
+    done
+    echo -ne "\n"
+done
